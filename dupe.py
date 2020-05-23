9266c28fad103c4ebf1418d851d89c7011c187b9
German Henriquez <germanandres.hv@gmail.com>
2020-05-22 23:37:50 -0400

    Compare method

--- dupe.py
+++ dupe.py
@@ -2,6 +2,7 @@
 Main library
 """
 from hashlib import md5
+from itertools import combinations
 from pathlib import Path
 
 
@@ -20,7 +21,19 @@ class FileBank:
         """
         for obj in self.path.glob('**/*'):  # https://docs.python.org/3/library/pathlib.html#pathlib.Path.glob
             if obj.is_file():
-                self.files.append(obj)
+                self.files.append(File(obj))
+
+    def run_comparisons(self):
+        """
+        Compares every file in self.files list
+        # TODO: Create a list or dictionary that lists every matching file
+        """
+        for f1, f2 in combinations(self.files, 2):
+            if f1.compare_to(f2) is True:
+                print(f'{f1.name} matches with {f2.name}')
+            else:
+                # print(f'{f1.name} does not match {f2.name}')
+                pass
 
 
 class File:
@@ -38,6 +51,13 @@ class File:
         self.file = file
         self.size = file.stat().st_size
         self._md5 = None
+
+    @property
+    def name(self) -> str:
+        """
+        Returns file name
+        """
+        return self.file.name
 
     def _calculate_md5(self) -> str:
         """
