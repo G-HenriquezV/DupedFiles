748089d1d887e1f8d2fc4a9e3ad7018bf26c2f4f
German Henriquez <germanandres.hv@gmail.com>
2020-05-24 19:09:14 -0400

    Working code

--- dupe.py
+++ dupe.py
@@ -1,37 +1,10 @@
 """
 Main library
 """
+import json
 from collections import defaultdict
 from hashlib import md5
 from pathlib import Path
-
-
-class FileBank:
-    """
-    Pass
-    """
-
-    def __init__(self, folder: str):
-        self.path = Path(folder)
-        self.files = defaultdict(list)
-
-    def scan_folders(self):
-        """
-        Appends every file in the folder and subfolders to the self.file list attribute
-        """
-        for obj in self.path.glob('**/*'):  # https://docs.python.org/3/library/pathlib.html#pathlib.Path.glob
-            if obj.is_file():
-                obj = FileToCheck(obj)
-                self.files[obj.hash].append(obj)
-
-    def print_duplicates(self) -> None:
-        """
-        Prints duplicate items
-        """
-        for key, value in self.files.items():
-            if len(value) > 1:
-                print(f'Duplicates found with md5 checksum {key}')
-                print(value)
 
 
 class FileToCheck:
@@ -66,6 +39,16 @@ class FileToCheck:
         """
         return self.file.name
 
+    @property
+    def location(self) -> str:
+        """
+        Returns the full location of the file as a string
+
+        :rtype: str
+        :return: Full string of the file location
+        """
+        return str(self.file.absolute())
+
     def _calculate_md5(self) -> str:
         """
         Calculates the md5 checksum of the file, sets the self._md5 attribute and returns it.
@@ -79,13 +62,12 @@ class FileToCheck:
         self._md5 = hash_md5.hexdigest()
         return self._md5
 
-    @property  # Maybe this should not be a property?
     def hash(self) -> str:
         """
-        Calculates the md5 checksum of the file if it
+        Calculates the md5 checksum of the file if it wasn't before. Then returns it.
 
-        :rtype: str
         :return: md5 checksum of the file
+        :rtype: str
         """
         if self._md5 is None:
             return self._calculate_md5()
@@ -101,4 +83,66 @@ class FileToCheck:
         if self.size != file.size:
             return False
         else:
-            return self.hash == file.hash
+            return self.hash() == file.hash()
+
+
+class FileBank:
+    """
+    TODO: Seriously I have to document everything here
+    """
+
+    def __init__(self, folder: str, autorun: bool = True):
+        """
+
+
+        :param folder: Folder to scan
+        :param autorun: If checksums and duplicates should be obtained when initialized
+        """
+        self.path = Path(folder)
+        self.files = defaultdict(list)
+        self.n_files = 0  # Number of files
+        self.duplicates = None
+        if autorun:
+            self.scan_folders()
+            self.get_duplicates()
+
+    def scan_folders(self):
+        """
+        Appends every file in the folder and subfolders to the self.file list attribute.
+        Automatically calculates the md5 checksum of every file
+        """
+        for obj in self.path.glob('**/*'):  # https://docs.python.org/3/library/pathlib.html#pathlib.Path.glob
+            if obj.is_file():
+                obj = FileToCheck(obj)
+                self.files[obj.hash()].append(obj)
+                self.n_files += 1
+
+    def get_duplicates(self) -> dict:
+        """
+        :return: Dictionary with duplicated files
+        """
+        dupe_dict = {}
+        for checksum, files in self.files.items():
+            if len(files) > 1:
+                dupe_dict[checksum] = files
+        self.duplicates = dupe_dict
+        return dupe_dict
+
+    def print_duplicates(self) -> None:
+        """
+        Prints duplicate items
+        """
+        for key, value in self.duplicates.items():
+            file_locations = map(lambda p: p.location, value)
+            print(f'MD5: {key}')
+            print('Files:')
+            for file in file_locations:
+                print(f'- {file}')
+            print('\n')
+
+    def save_json(self) -> None:
+        """
+        Saves a json file, named duplicates.json, at the cwd with every duplicated file.
+        """
+        with open('duplicates.json', 'w', encoding='utf-8') as f:
+            json.dump(self.duplicates, f, indent=2, ensure_ascii=False)
